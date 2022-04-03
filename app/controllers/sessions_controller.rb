af88eb9954844199addc4df01f399085a62f32fd
IBRAHIMA <hadi.ibrahima8@gmail.com>
2022-04-03 06:38:04 +0100

    applicationfacebookcloneprojet

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+skip_before_action :login_required, only: [:new, :create]
+  def new
+  end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+      if user && user.authenticate(params[:session][:password])
+        session[:user_id] = user.id
+     redirect_to blog_path(user.id)
+      else
+        flash.now[:danger] = 'adresse e-mail ou mot de passe incorrect'
+      render :new
+   end
+  end
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'Déconnecté'
+    redirect_to new_session_path
+  end
+end
